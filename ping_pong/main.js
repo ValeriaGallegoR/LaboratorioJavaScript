c05c5697e1499de32ddc4ae3153e30e7eccac274
ValeriaGallegoR <valeriagallego20@gmail.com>
2021-09-07 22:46:16 -0500

    Se crea el proyecto con las funciones bar, board y boardView (plantillas para ver el modelo en la web)

---
+++ ping_pong/main.js
@@ -0,0 +1,62 @@
+(function(){
+
+    Window.board = function (width, height){
+    this.width = width;
+    this.height = height;
+    this.playing = false;
+    this.game_over = false;
+    this.bars = [];
+    this.ball = null;
+
+}
+
+Window.board.prototype = {
+    get elements(){
+        var elements = this.bars;
+        elements.push(this.ball);
+        return elements;
+    }
+}
+})();
+
+(function(){
+Window.bar = function(ejeX, ejeY, width, height, board){
+    this.ejeX = ejeX;
+    this.ejeY = ejeY;
+    this.width = width;
+    this.height = height;
+    this.board = board;
+    this.board.bars.push(this);
+    this.kind = "rectangle";
+}
+
+Window.bar.prototype = {
+    down: function(){
+
+    },
+    up: function(){
+    }
+}
+})();
+
+(function(){
+    Window.boardView = function(canvas,board){
+    canvas = document.getElementById("canvas");
+    // canvas = CanvasRenderingContext2D;
+    this.width = board.width;
+    this.height = board.height;
+    this.board = board;
+    this.ctx = canvas.getContext("2d");
+    canvas.style.width = "800px";
+    canvas.style.height = "400px";
+}
+
+Window.boardView.prototype = {
+    draw: function(){
+        for (var i = this.board.elements.length - 1; i>= 0; i--){
+             var el=  this.board.elements[i];
+             draw(this.ctx, el);
+        };
+    }
+ }
+});
