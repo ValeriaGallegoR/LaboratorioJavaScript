2df0165028dc7b81f08adc200d7dbabfad5be399
ValeriaGallegoR <valeriagallego20@gmail.com>
2021-09-08 21:22:32 -0500

    Se agregaron funcionalidades de collision

--- ping_pong/main.js
+++ ping_pong/main.js
@@ -22,13 +22,16 @@
 
 (function () {
   self.Ball = function (x, y, radius, board) {
+    //Crea el objeto pelota y le da movimiento
     this.x = x;
     this.y = y;
     this.radius = radius;
     this.speed_y = 0;
     this.speed_x = 3;
     this.board = board;
-    this.direction = 1;
+    this.direction = -1;
+    this.bounce_angle = 0;
+    this.max_bounce_angle = Math.PI / 12;
     this.speed = 3;
 
     board.ball = this;
@@ -39,11 +42,32 @@
       this.x += this.speed_x * this.direction;
       this.y += this.speed_y;
     },
+    get width() {
+      return this.radius * 2;
+    },
+    get height() {
+      return this.radius * 2;
+    },
+    collision: function (bar) {
+      //Reacciona a la colisión con una barra que recibe como parámetro
+      var relative_intersect_y = bar.y + bar.height / 2 - this.y;
+
+      var normalized_intersect_y = relative_intersect_y / (bar.height / 2);
+
+      this.bounce_angle = normalized_intersect_y * this.max_bounce_angle;
+      console.log(this.bounce_angle);
+      this.speed_y = this.speed * -Math.sin(this.bounce_angle);
+      this.speed_x = this.speed * Math.cos(this.bounce_angle);
+
+      if (this.x > this.board.width / 2) this.direction = -1;
+      else this.direction = 1;
+    },
   };
 })();
 
 (function () {
   self.Bar = function (x, y, width, height, board) {
+    // Crea los objetos de barra en el eje y
     this.x = x;
     this.y = y;
     this.width = width;
@@ -51,7 +75,7 @@
     this.board = board;
     this.board.bars.push(this);
     this.kind = "rectangle";
-    this.speed = 10;
+    this.speed = 5;
   };
 
   self.Bar.prototype = {
@@ -61,9 +85,9 @@
     up: function () {
       this.y -= this.speed;
     },
-    // toString: function () {
-    //   return "x: " + this.x + "y: " + this.y;
-    // },
+    toString: function () {
+      return "x: " + this.x + " y: " + this.y;
+    },
   };
 })();
 
@@ -87,16 +111,46 @@
         draw(this.ctx, el);
       }
     },
+    check_collisions: function () {
+      for (var i = this.board.bars.length - 1; i >= 0; i--) {
+        var bar = this.board.bars[i];
+        if (hit(bar, this.board.ball)) {
+          this.board.ball.collision(bar);
+        }
+      }
+    },
     play: function () {
       if (this.board.playing) {
         this.clean();
         this.draw();
+        this.check_collisions();
         this.board.ball.move();
       }
     },
   };
 
+  function hit(a, b) {
+    //Revisa si a colisiona con b
+    var hit = false;
+    //Colsiones horizontales
+    if (b.x + b.width >= a.x && b.x < a.x + a.width) {
+      //Colisiones verticales
+      if (b.y + b.height >= a.y && b.y < a.y + a.height) hit = true;
+    }
+    //Colisión de a con b
+    if (b.x <= a.x && b.x + b.width >= a.x + a.width) {
+      if (b.y <= a.y && b.y + b.height >= a.y + a.height) hit = true;
+    }
+    //Colisión b con a
+    if (a.x <= b.x && a.x + a.width >= b.x + b.width) {
+      if (a.y <= b.y && a.y + a.height >= b.y + b.height) hit = true;
+    }
+
+    return hit;
+  }
+
   function draw(ctx, element) {
+    //Dibuja en la pantalla los objetos barra y pelota
     switch (element.kind) {
       case "rectangle":
         ctx.fillRect(element.x, element.y, element.width, element.height);
@@ -117,29 +171,32 @@ var bar = new Bar(20, 100, 40, 100, board);
 var bar_2 = new Bar(735, 100, 40, 100, board);
 var canvas = document.getElementById("canvas");
 var board_view = new BoardView(canvas, board);
-var ball = new Ball(300, 100, 10, board);
+var ball = new Ball(350, 100, 10, board);
 
 document.addEventListener("keydown", function (ev) {
-  ev.preventDefault();
-  if (ev.KeyCode == 38) {
+  if (ev.keyCode == 38) {
+    ev.preventDefault();
     bar.up();
-  } else if (ev.keyCode == 40) {
+  } else if (ev.keyCode === 40) {
+    ev.preventDefault();
     bar.down();
-  } else if (ev.keyCode == 87) {
+  } else if (ev.keyCode === 87) {
+    ev.preventDefault();
     bar_2.up();
-  } else if (ev.keyCode == 83) {
+  } else if (ev.keyCode === 83) {
+    ev.preventDefault();
     bar_2.down();
-  } else if (ev.keyCode == 32) {
-    bar_2.down();
+  } else if (ev.keyCode === 32) {
+    ev.preventDefault();
+
     board.playing = !board.playing;
   }
-  console.log(bar_2.toString());
 });
 
 board_view.draw();
-window.addEventListener("load", controller);
+window.requestAnimationFrame(controller);
 
 function controller() {
   board_view.play();
-  window.requestAnimationFrame(controller);
+  requestAnimationFrame(controller);
 }
